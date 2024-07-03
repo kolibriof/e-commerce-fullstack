aba6066d7df58dae959cadc177d5654317928b20
Dima Savchenko <kolibri380@gmail.com>
2024-07-03 18:34:17 +0200

    guard and service setup

---
+++ ecommerce-fe/src/app/guards/AuthGuard.ts
@@ -0,0 +1,31 @@
+import {
+	ActivatedRouteSnapshot,
+	CanActivate,
+	GuardResult,
+	MaybeAsync,
+	Router,
+	RouterStateSnapshot,
+} from "@angular/router";
+import { LoginService } from "../services/login-service";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class AuthGuard implements CanActivate {
+	constructor(private loginService: LoginService, private router: Router) {}
+
+	canActivate(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot,
+	): MaybeAsync<GuardResult> {
+		return this.loginService.getAuthorization().pipe(
+			map((authorized) => {
+				if (!authorized) {
+					this.router.navigate([""]);
+				}
+				console.log("call");
+				return authorized;
+			}),
+		);
+	}
+}
---
+++ ecommerce-fe/src/app/services/login-service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class LoginService {
+	private readonly url = "http://localhost:8080";
+	private authorized = new BehaviorSubject<boolean>(false);
+
+	constructor(private http: HttpClient) {}
+
+	getAuthorization() {
+		return this.authorized.asObservable();
+	}
+
+	loginUser(creds: any) {
+		return this.http.post(this.url + "/login", creds.form.value, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
+
+	createUser(creds: any) {
+		return this.http.post(this.url + "/users", creds.form.value, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
+}
