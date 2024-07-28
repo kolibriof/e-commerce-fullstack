6389fef0d61710f76da96d87c774bdfbca02a823
Dima Savchenko <dsavc3@softserveinc.com>
2024-07-28 02:15:30 +0200

    minor bug fixes

--- ecommerce-be/src/main/java/com/ecommerce/ecommerce_be/security/SecurityConfig.java
+++ ecommerce-be/src/main/java/com/ecommerce/ecommerce_be/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ecommerce.ecommerce_be.security;
 
-import com.ecommerce.ecommerce_be.repos.SessionRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.session.SimpleRedirectSessionInformationExpiredStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -26,11 +26,7 @@ public class SecurityConfig {
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                         .maximumSessions(1)
-                        .expiredSessionStrategy(event -> {
-                            event.getResponse().sendRedirect("/login");
-                            event.getResponse().setContentType("application/json");
-                            event.getResponse().sendError(440, "Session has expired! Please login!");
-                        })
+                        .expiredSessionStrategy(new SimpleRedirectSessionInformationExpiredStrategy("/login"))
                 );
 
         return http.build();
